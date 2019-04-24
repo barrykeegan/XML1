cbb491ad957a4c9eb11f1d935cbab88c429645aa
barrykeegan <barry.keegan@gmail.com>
2019-04-24 15:10:51 +0100

    Add project files.

---
+++ XML1/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XML1.Models;
+using System.Data;
+
+namespace XML1.Controllers
+{
+    public class HomeController : Controller
+    {
+        static DataSet ds;
+        static DataTable dt;
+        // GET: Home
+        public ActionResult Index()
+        {
+            if(System.IO.File.Exists(Server.MapPath("~/App_Data/review.xml")))
+            {
+                ds = new DataSet();
+                ds.ReadXml(Server.MapPath("~/App_Data/review.xml"));
+                dt = ds.Tables["Course"];
+                Response.Write(dt.Rows.Count);
+            }
+            else
+            {
+                ds = new DataSet("Courses");
+                dt = new DataTable("Course");
+                dt.Columns.Add("Code");
+                dt.Columns.Add("Title");
+                dt.Columns.Add("Type");
+                dt.Columns.Add("Credits");
+                dt.Columns.Add("ValidationDate");
+                ds.Tables.Add(dt);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                DataRow row = dt.NewRow();
+                row["Code"] = course.CourseCode;
+                row["Title"] = course.CourseTitle;
+                row["Type"] = course.CourseType;
+                row["Credits"] = course.CourseCredits;
+                row["ValidationDate"] = course.CourseValidationDate;
+                dt.Rows.Add(row);
+                ds.AcceptChanges();
+                ds.WriteXml(Server.MapPath("~/App_Data/review.xml"));
+                return View();
+            }
+            else
+            {
+                return View(course);
+            }
+            
+            
+        }
+    }
+}
---
+++ XML1/Models/Course.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace XML1.Models
+{
+    public enum CourseTypeEnum
+    {
+        FullTime,
+        PartTime
+    }
+
+    public class Course
+    {
+        [Display(Name = "Course Code")]
+        [Required]        
+        public string CourseCode { get; set; }
+        [Required]
+        [Display(Name = "Course Title")]
+        public string CourseTitle { get; set; }
+        [Required]
+        [Display(Name = "Course Type")]
+        public CourseTypeEnum CourseType { get; set; }
+        [Required]
+        [Display(Name = "Credits")]
+        public int CourseCredits { get; set; }
+        [Required]
+        [Display(Name = "Validation Date")]        
+        [Range(2000,2019)]
+        public int CourseValidationDate { get; set; }
+
+    }
+}
